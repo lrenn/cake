e5a32dbbf1f6ba5015f5a3d835e4044f68dfcbe4
Luke Renn <luke.renn@gmail.com>
2010-10-05 22:58:15 -0400

    Initial maven->ivy poc rebased to master after the f branch merge.

--- project.clj
+++ project.clj
@@ -5,4 +5,4 @@
                  [ordered-set "0.1.0"]
                  [com.jcraft/jsch "0.1.42"]
                  [org.apache.ant/ant "1.8.1"]
-                 [org.clojars.ninjudd/maven-ant-tasks "2.1.0" :exclusions [ant/ant]]])
+                 [org.apache.ivy/ivy "2.2.0-rc1"]])
